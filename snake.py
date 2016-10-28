3f1347ccbef155725cc1fa084a22bed3c835ecd7
John Kamalu <john.kamalu@gmail.com>
2016-10-28 01:21:34 -0700

    Extension of starter code to model baseline and oracle
    Features added: Multiple moving mice,
                    modularized game class,
                    "dumb" baseline snake which seeks one mouse.
    
    TODO: Add hard walls to movement and rules,
          other features,
          clean up code.

--- snake.py
+++ snake.py
@@ -7,20 +7,18 @@ import random
 
 from pygame.locals import *
 
-FPS = 5
+FPS = 15
 pygame.init()
-fpsClock=pygame.time.Clock()
+fpsClock = pygame.time.Clock()
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 surface = pygame.Surface(screen.get_size())
 surface = surface.convert()
-surface.fill((255,255,255))
+surface.fill((0, 0, 0))
 clock = pygame.time.Clock()
 
-pygame.key.set_repeat(1, 40)
-
-GRIDSIZE=10
+GRIDSIZE = 10
 GRID_WIDTH = SCREEN_WIDTH / GRIDSIZE
 GRID_HEIGHT = SCREEN_HEIGHT / GRIDSIZE
 UP    = (0, -1)
@@ -34,18 +32,28 @@ def draw_box(surf, color, pos):
     r = pygame.Rect((pos[0], pos[1]), (GRIDSIZE, GRIDSIZE))
     pygame.draw.rect(surf, color, r)
 
+def manhattan_distance(pos1, pos2):
+    dx = pos1[0] - pos2[0]
+    dy = pos1[1] - pos2[1]
+    return abs(dx) + abs(dy)
+
 class Snake(object):
     def __init__(self):
         self.lose()
-        self.color = (0,0,0)
+        self.color = (0, 128, 0)
+        self.cur_target = None
 
     def get_head_position(self):
         return self.positions[0]
 
     def lose(self):
         self.length = 1
+        self.mice_eaten = 0
         self.positions =  [((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))]
         self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
+
+    def grow(self, addendum):
+        self.length += addendum
 
     def point(self, pt):
         if self.length > 1 and (pt[0] * -1, pt[1] * -1) == self.direction:
@@ -56,7 +64,8 @@ class Snake(object):
     def move(self):
         cur = self.positions[0]
         x, y = self.direction
-        new = (((cur[0]+(x*GRIDSIZE)) % SCREEN_WIDTH), (cur[1]+(y*GRIDSIZE)) % SCREEN_HEIGHT)
+        new = (((cur[0] + (x * GRIDSIZE)) % SCREEN_WIDTH), \
+                (cur[1] + (y * GRIDSIZE)) % SCREEN_HEIGHT)
         if len(self.positions) > 2 and new in self.positions[2:]:
             self.lose()
         else:
@@ -68,50 +77,116 @@ class Snake(object):
         for p in self.positions:
             draw_box(surf, self.color, p)
 
-class Apple(object):
-    def __init__(self):
-        self.position = (0,0)
-        self.color = (255,0,0)
+class Mouse(object):
+    def __init__(self, reward = 3):
+        self.color = (255, 255, 255)
+        self.position = (0, 0)
         self.randomize()
+        self.reward = reward
 
     def randomize(self):
-        self.position = (random.randint(0, GRID_WIDTH-1) * GRIDSIZE, random.randint(0, GRID_HEIGHT-1) * GRIDSIZE)
+        self.position = (random.randint(0, GRID_WIDTH-1) * GRIDSIZE, \
+                         random.randint(0, GRID_HEIGHT-1) * GRIDSIZE)
+
+    def move(self, rand = True):
+        if rand:
+            cur = self.position
+            x, y = random.choice([UP, DOWN, LEFT, RIGHT])
+            self.position = (((cur[0] + (x * GRIDSIZE)) % SCREEN_WIDTH), \
+                              (cur[1] + (y * GRIDSIZE)) % SCREEN_HEIGHT)
+        else:
+            # TODO: Provide alternative to random movement
+            pass
 
     def draw(self, surf):
         draw_box(surf, self.color, self.position)
 
-def check_eat(snake, apple):
-    if snake.get_head_position() == apple.position:
-        snake.length += 1
-        apple.randomize()
+class Game(object):
+    def __init__(self, mice_count = 5):
+        self.snake = Snake()
+        self.mice = []
+        for i in range(mice_count):
+            self.mice.append(Mouse()) 
+        self.move_parity = 0
+        self.lose()
+
+    def lose(self):
+        self.snake.lose()
+
+    def check_eat(self, snake, mouse):
+        if snake.get_head_position() == mouse.position:
+            snake.grow(mouse.reward)
+            snake.cur_target = None
+            mouse.randomize()
+            snake.mice_eaten += 1
+
+    def orient_snake(self):
+        if self.snake.cur_target == None:
+            closest_mouse = min(self.mice, key=lambda mouse: \
+                                manhattan_distance(mouse.position, self.snake.get_head_position())) 
+            self.snake.cur_target = closest_mouse
+        else:
+            closest_mouse = self.snake.cur_target
+        snake_pos = self.snake.get_head_position()
+        mouse_pos = closest_mouse.position
+        dx = snake_pos[0] - mouse_pos[0]
+        dy = snake_pos[1] - mouse_pos[1]
+        if dy > 0:
+            # going up ?
+            if dx > 0:
+                # going left ?
+                if abs(dy) > abs(dx):
+                    direction = UP if self.snake.direction != DOWN else LEFT
+                else:
+                    direction = LEFT if self.snake.direction != RIGHT else UP
+            else:
+                # going right ?
+                if abs(dy) > abs(dx):
+                    direction = UP if self.snake.direction != DOWN else RIGHT
+                else:
+                    direction = RIGHT if self.snake.direction != LEFT else UP
+        else:
+            # going down ?
+            if dx > 0:
+                # going left ?
+                if abs(dy) > abs(dx):
+                    direction = DOWN if self.snake.direction != UP else LEFT
+                else:
+                    direction = LEFT if self.snake.direction != RIGHT else DOWN
+            else:
+                # going right ?
+                if abs(dy) > abs(dx):
+                    direction = DOWN if self.snake.direction != UP else RIGHT
+                else:
+                    direction = RIGHT if self.snake.direction != LEFT else DOWN
+        self.snake.point(direction)
+
+    def tick(self):
+        self.orient_snake()
+        self.snake.move()
+        for mouse in self.mice:
+            if self.move_parity:
+                mouse.move()
+                self.move_parity = 0
+            else:
+                self.move_parity = 1
+            self.check_eat(self.snake, mouse)
+            self.snake.draw(surface)
+            mouse.draw(surface)
+        
 
 if __name__ == '__main__':
-    snake = Snake()
-    apple = Apple()
-    while True:
+    game = Game()
 
+    while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-            elif event.type == KEYDOWN:
-                if event.key == K_UP:
-                    snake.point(UP)
-                elif event.key == K_DOWN:
-                    snake.point(DOWN)
-                elif event.key == K_LEFT:
-                    snake.point(LEFT)
-                elif event.key == K_RIGHT:
-                    snake.point(RIGHT)
-
-
-        surface.fill((255,255,255))
-        snake.move()
-        check_eat(snake, apple)
-        snake.draw(surface)
-        apple.draw(surface)
+        surface.fill((0, 0, 0))
+        game.tick()
         font = pygame.font.Font(None, 36)
-        text = font.render(str(snake.length), 1, (10, 10, 10))
+        text = font.render(str(game.snake.mice_eaten), 1, (255, 255, 255))
         textpos = text.get_rect()
         textpos.centerx = 20
         surface.blit(text, textpos)
@@ -119,4 +194,4 @@ if __name__ == '__main__':
 
         pygame.display.flip()
         pygame.display.update()
-        fpsClock.tick(FPS + snake.length/3)
+        fpsClock.tick(FPS)
