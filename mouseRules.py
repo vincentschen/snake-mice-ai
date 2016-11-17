c633be6a231a96547022129ff80cb53339a1b082
Vincent <vincentschen@yahoo.com>
2016-11-17 02:47:44 -0800

    Add snakeAgent and mouseAgent

---
+++ mouseRules.py
@@ -0,0 +1,32 @@
+"""
+Handles state changes for mice agents
+"""
+
+def getLegalActions(state, agentIndex):
+    possible_results = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    results = []
+    agentPos = state.getMicePositions()[agentIndex-1]
+    for move in possible_results:
+      if isValidLocation(state, (agentPos[0] + move[0], agentPos[1] + move[1])):
+        results.append(move)
+    return results
+
+def applyAction(state, action, agentIndex):
+    newLoc = (state.micePositions[agentIndex-1] + action[0], state.micePositions[agentIndex-1] + action[1])
+    state.micePositions[agentIndex-1] = newLoc
+
+#valid location is any square in the grid without a mouse or snake tile
+def isValidLocation(state, location):
+    if location[0] >= state.dimensions[0] or location[1] >= state.dimensions[1]:
+      return False
+    for loc in [state.getSnakePositions() + state.getMicePositions()]:
+      if loc == location:
+        return False
+    return True
+
+#returns a random location on the board not taken up by snake or mice
+def randomLocation(state):
+    while True:
+      possible = (random.randint(0, state.dimensions[0] - 1), random.randint(0, state.dimensions[1] - 1))
+      if isValidLocation(possible):
+        return possible
---
+++ snakeRules.py
@@ -0,0 +1,42 @@
+"""
+Handles state changes for snake agent
+"""
+
+def getLegalActions(state):
+    possible_results = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    results = []
+    agentPos = state.getSnakePositions()[0]
+    for move in possible_results:
+      if isValidLocation(state, (agentPos[0] + move[0], agentPos[1] + move[1])):
+        results.append(move)
+    return results
+
+# valid location is any square in the grid, 
+# even if there's a mouse or snake tile there
+def isValidLocation(state, location):
+    if location[0] >= state.dimensions[0] or location[1] >= state.dimensions[1]:
+      return False
+    for loc in [state.getSnakePositions()]:
+      if loc == location:
+        return False
+    return True
+
+def applyAction(state, action):
+    newLoc = (state.getSnakePositions()[0][0] + action[0], state.getSnakePositions()[0][1] + action[1])
+    # itself or a wall, game over
+    if not isValidLocation(newLoc):
+      state.isLose = True
+    # new tile has a mouse
+    elif newLoc in state.getMicePositions():
+      eatenMouseIndex = state.getMicePositions().index(newLoc)
+      state.micePositions[eatenMouseIndex] = MouseRules.randomLocation(state)
+      state.snakePositions.insert(0, newLoc)
+      state.score += MOUSE_REWARD
+      state.miceEaten += 1
+      if state.miceEaten >= MICE_TO_WIN:
+        state.isWin = True
+    #new tile is empty
+    else:   
+      state.snakePositions.insert(0, newLoc)
+      state.snakePositions.pop()
+      state.score -= MOVE_PENALTY 
