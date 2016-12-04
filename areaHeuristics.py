e98b47f68442f9ab3e6b8ba108b076d6982e3be9
John Kamalu <john.kamalu@gmail.com>
2016-12-03 19:21:26 -0800

    Wrote the snake polygon area and inaccessible positions heuristics

--- areaHeuristics.py
+++ areaHeuristics.py
@@ -1,9 +1,14 @@
 from shapely import geometry
 from collections import defaultdict
+from collections import deque
 import heapq
 
-# takes snake positions contained in state variable
-def getCornerHash(snakePositions):
+def getSnakeCornersHashedByX(snakePositions):
+    """
+    This function returns a dictionary mapping x-coordinate values to 
+    lists of corner coordinates (snake positions) in a heapq configuration
+    for which this value is the x-coordinate
+    """
     xHashed = defaultdict(list)
     heapq.heappush(xHashed[snakePositions[0][0]], snakePositions[0]) 
     heapq.heappush(xHashed[snakePositions[-1][0]], snakePositions[-1]) 
@@ -20,21 +25,78 @@ def getCornerHash(snakePositions):
             pass
     return xHashed
 
-def getCornerPolygon(snakePositions):
-    snakePositionsByX = defaultdict(list)
-    for snakePos in snakePositions:
-        heapq.heappush(snakePositionsByX[snakePos[0]], snakePos) 
-    sortedXIndices = sorted(snakePositionsByX)
-    leftToTop = sorted(snakePositionsByX[sortedXIndices[0]])
-    rightToBottom = []
-    for i in range(1, len(sortedXIndices) - 1):
-        positionsGivenX = snakePositionsByX[sortedXIndices[i]]
-        leftToTop += heapq.nlargest(1, positionsGivenX)
-        if len(positionsGivenX) > 1:
-            rightToBottom += heapq.nsmallest(1, positionsGivenX)
-    rightToBottom += sorted(snakePositionsByX[sortedXIndices[-1]])
-    rightToBottom.reverse()
-    return geometry.Polygon([[p[0], p[1]] for p in (leftToTop + rightToBottom)])
+def getSnakePolygon(snakePositions):
+    """
+    This function returns a polygon as defined in the shapely library
+    composed of the points defining the perimeter of the snake for the
+    x and y coorindate value ranges that it occupies
+    """
+    # dict of x values to lists of positions with that x value
+    # sorted by increasing y value
+    snakePositionsByRow = defaultdict(list)
+    for snakeRow, snakeCol in snakePositions:
+        heapq.heappush(snakePositionsByRow[snakeRow], (snakeRow, snakeCol)) 
+    sortedRows = sorted(snakePositionsByRow)
+    topPositions = snakePositionsByRow[sortedRows[0]]
+    rightPositions = []
+    bottomPositions = snakePositionsByRow[sortedRows[-1]]
+    leftPositions = deque()
+    # fill topPositions and bottomPositions in one pass, L to R
+    for row in sortedRows[1:-1]:
+        positionsGivenRow = snakePositionsByRow[row]
+        rightPositions.append(heapq.nlargest(1, positionsGivenRow)[0])
+        # no double counting
+        if len(positionsGivenRow) > 1:
+            leftPositions.appendleft(heapq.nsmallest(1, positionsGivenRow)[0])
+    bottomPositions.reverse()
+    perimeterPositions = topPositions + rightPositions + bottomPositions
+    perimeterPositions.extend(leftPositions)
+    print perimeterPositions
+    return geometry.Polygon([[p[0], p[1]] for p in perimeterPositions])
 
-snakePositions = [(5,0), (4,0), (3,0), (2,0), (1,0), (0,0), (0,1), (0,2), (0,3), (0,4), (1,4), (2,4), (3,4), (4,4), (4,3), (4,2), (4,1), (5,1), (6,1), (7,1)]
-print getCornerPolygon(snakePositions).area
+def isAValidLocation(pos, snakePositions, dimensions):
+    """
+    A helper function for the updateAcceisblePoints() recurrence, returns
+    whether a position exists on the board and is not occupied by the
+    snake
+    """
+    return pos[0] >= 0 and pos[0] < dimensions[0] and \
+           pos[1] >= 0 and pos[1] < dimensions[1] and \
+           pos not in snakePositions
+
+def updateAccessiblePoints(start, accessiblePositions, snakePositions, dimensions):
+    """
+    A recursive search which updates the set accessiblePoints with all
+    positions accessible form the start position, given the current snake
+    potisions and the dimensions
+    """
+    startRow, startCol = start
+    nextPositions = [(startRow, startCol + 1), (startRow, startCol - 1), (startRow + 1, startCol), (startRow - 1, startCol)]
+    for nextPos in nextPositions:
+        if isAValidLocation(nextPos, snakePositions, dimensions) and nextPos not in accessiblePositions:
+            print nextPos
+            accessiblePositions.add(nextPos)
+            updateAccessiblePoints(nextPos, accessiblePositions, snakePositions, dimensions)
+
+def getAreaBlockedBySnake(snakeList, dimensions):
+    """
+    This functions returns the 
+    """
+    accessiblePositions = set()
+    snakePositions = set(snakeList)
+    rows, cols = dimensions
+    edgePositions = set()
+    for col in range(cols):
+        edgePositions.add((0, col))
+        edgePositions.add((rows, col))
+    for row in range(rows):
+        edgePositions.add((row, 0))
+        edgePositions.add((row, cols))
+    for edge in edgePositions:
+        if edge in accessiblePositions or edge in snakePositions:
+            continue
+        updateAccessiblePoints(edge, accessiblePositions, snakePositions, dimensions)
+    return rows * cols - len(accessiblePositions)
+
+# testing input
+# snakePositions = [(5,0), (4,0), (3,0), (2,0), (1,0), (0,0), (0,1), (0,2), (0,3), (0,4), (1,4), (2,4), (3,4), (4,4), (4,3), (4,2), (4,1), (5,1), (6,1), (7,1)]
