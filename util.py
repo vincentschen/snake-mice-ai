da62a3f27b5f63c9e84faa413259a80583f4eb34
John Kamalu <john.kamalu@gmail.com>
2016-12-03 19:31:08 -0800

    Moved heuristic functions in areaHeuristics.py to util.py, as well as import statements

--- util.py
+++ util.py
@@ -1,3 +1,8 @@
+from shapely import geometry
+from collections import defaultdict
+from collections import deque
+import heapq
+
 def getStraightLength(self, state, action):
   #CASE 1: Snake of length 1
   if len(state.snakePositions) == 1: #here any action will result in a straight length of 2
@@ -58,4 +63,107 @@ def k_norm(head, points):
     distances_to_points = sorted([distance(head, point) for point in points])
     summation = sum([distances_to_points[i] ** (i + 1) for i in range(len(points))]) 
     norm = summation ** (1.0 / len(points))
-    return norm
+    return norm 
+
+# TODO: Use this function for some heuristic?
+def getSnakeCornersHashedByX(snakePositions):
+    """
+    This function returns a dictionary mapping x-coordinate values to 
+    lists of corner coordinates (snake positions) in a heapq configuration
+    for which this value is the x-coordinate
+    """
+    xHashed = defaultdict(list)
+    heapq.heappush(xHashed[snakePositions[0][0]], snakePositions[0]) 
+    heapq.heappush(xHashed[snakePositions[-1][0]], snakePositions[-1]) 
+    for i in range(len(snakePositions)):
+        snakeX1, snakeY1 = snakePositions[i]
+        try:
+            snakeX2, snakeY2 = snakePositions[i + 2]
+            xDiff = snakeX1 - snakeX2
+            yDiff = snakeY1 - snakeY2
+            if (xDiff == 1 or xDiff == -1) and (yDiff == 1 or yDiff == -1):
+                corner = snakePositions[i + 1]
+                heapq.heappush(xHashed[corner[0]], corner)
+        except IndexError:
+            pass
+    return xHashed
+
+def getSnakePolygon(snakePositions):
+    """
+    This function returns a polygon as defined in the shapely library
+    composed of the points defining the perimeter of the snake for the
+    x and y coorindate value ranges that it occupies
+
+    Area ($area$) is an instance variable of the polygon
+    """
+    # dict of x values to lists of positions with that x value
+    # sorted by increasing y value
+    snakePositionsByRow = defaultdict(list)
+    for snakeRow, snakeCol in snakePositions:
+        heapq.heappush(snakePositionsByRow[snakeRow], (snakeRow, snakeCol)) 
+    sortedRows = sorted(snakePositionsByRow)
+    topPositions = snakePositionsByRow[sortedRows[0]]
+    rightPositions = []
+    bottomPositions = snakePositionsByRow[sortedRows[-1]]
+    leftPositions = deque()
+    # fill topPositions and bottomPositions in one pass, L to R
+    for row in sortedRows[1:-1]:
+        positionsGivenRow = snakePositionsByRow[row]
+        rightPositions.append(heapq.nlargest(1, positionsGivenRow)[0])
+        # no double counting
+        if len(positionsGivenRow) > 1:
+            leftPositions.appendleft(heapq.nsmallest(1, positionsGivenRow)[0])
+    bottomPositions.reverse()
+    perimeterPositions = topPositions + rightPositions + bottomPositions
+    perimeterPositions.extend(leftPositions)
+    print perimeterPositions
+    return geometry.Polygon([[p[0], p[1]] for p in perimeterPositions])
+
+def isAValidLocation(pos, snakePositions, dimensions):
+    """
+    A helper function for the updateAcceisblePoints() recurrence, returns
+    whether a position exists on the board and is not occupied by the
+    snake
+    """
+    return pos[0] >= 0 and pos[0] < dimensions[0] and \
+           pos[1] >= 0 and pos[1] < dimensions[1] and \
+           pos not in snakePositions
+
+def updateAccessiblePositions(start, accessiblePositions, snakePositions, dimensions):
+    """
+    A recursive search which updates the set accessiblePositions with all
+    positions accessible form the start position, given the current snake
+    potisions and the dimensions
+    """
+    startRow, startCol = start
+    nextPositions = [(startRow, startCol + 1), (startRow, startCol - 1), (startRow + 1, startCol), (startRow - 1, startCol)]
+    for nextPos in nextPositions:
+        if isAValidLocation(nextPos, snakePositions, dimensions) and nextPos not in accessiblePositions:
+            print nextPos
+            accessiblePositions.add(nextPos)
+            updateAccessiblePositions(nextPos, accessiblePositions, snakePositions, dimensions)
+
+def getAreaBlockedBySnake(snakeList, dimensions):
+    """
+    This functions returns the sum area currently accessible from
+    every edge position on the board, where accessibility is defined 
+    such that you can reach one position from another without crossing 
+    the snake.
+
+    Depends on updateAccessiblePositions() and isAValidLocation()
+    """
+    accessiblePositions = set()
+    snakePositions = set(snakeList)
+    rows, cols = dimensions
+    edgePositions = set()
+    for col in range(cols):
+        edgePositions.add((0, col))
+        edgePositions.add((rows, col))
+    for row in range(rows):
+        edgePositions.add((row, 0))
+        edgePositions.add((row, cols))
+    for edge in edgePositions:
+        if edge in accessiblePositions or edge in snakePositions:
+            continue
+        updateAccessiblePositions(edge, accessiblePositions, snakePositions, dimensions)
+    return rows * cols - len(accessiblePositions)
