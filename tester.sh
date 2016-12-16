f335a6d4600d332dcd75dfa6bd1868de73072150
Scott Kaz <root@DN200o8o7.stanford.edu>
2016-12-16 03:38:54 -0800

    changed tester.sh

--- tester.sh
+++ tester.sh
@@ -1,3 +1,4 @@
+echo "TEST NOTE: Preliminary test of everything..."
 python game.py -s greedy -n 1 -q
 python game.py -s oracle -n 1 -q
 python game.py -s expectimax -n 1 -d 2 -e a -q
@@ -5,3 +6,4 @@ python game.py -s expectimax -n 1 -d 2 -e b -q
 python game.py -s expectimax -n 1 -d 2 -e c -q
 python game.py -s minimax -n 1 -d 2 -e b -q
 python game.py -s alphabeta -n 1 -d 2 -e b -q
+echo "DONE!"
